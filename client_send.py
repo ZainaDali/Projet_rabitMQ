47932b2412d720f8fbbbe1fe09465cd8aa823697
imana173 <imana173@gmail.com>
2025-04-29 16:42:07 +0200

    Ajout de l'amelioration Exchange

--- client_send.py
+++ client_send.py
@@ -9,9 +9,10 @@ parameters = pika.ConnectionParameters('infoexpertise.hopto.org', 5672, '/', cre
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 
-channel.queue_declare(queue='calculsZI_DZ')
+# Déclarer l'exchange direct
+channel.exchange_declare(exchange='calculs_exchange', exchange_type='direct')
 
-OPERATIONS = ['add', 'sub', 'mul', 'div', 'all']
+OPERATIONS = ['add', 'sub', 'mul', 'div']
 
 def generate_message():
     n1 = random.randint(1, 100)
@@ -27,8 +28,10 @@ def generate_message():
 
 while True:
     message = generate_message()
-    channel.basic_publish(exchange='',
-                          routing_key='calculs',
-                          body=json.dumps(message))
+    channel.basic_publish(
+        exchange='calculs_exchange',
+        routing_key=message['op'],  # Utilise add/sub/mul/div comme routing key
+        body=json.dumps(message)
+    )
     print(f"Envoyé: {message}")
     time.sleep(random.uniform(2, 5))
